676bc645b9ba6e99611e7973e5382d4749f1c80a
Andrew Lai <arjlai221@gmail.com>
2022-02-26 17:58:01 -0800

    added option to command line

--- top_compiler.py
+++ top_compiler.py
@@ -4,18 +4,25 @@ import subprocess
 from json_parser import parse
 from type_checker import typeCheckProgram
 from generateLLVM import toLLVM
+from cfgToSSA import topSSACompile
 from ast_class_definitions import m_prog
 
-def top_compile(miniFile, outputFile = 'compilerOutput.ll'):
+def top_compile(miniFile, outputFile = 'compilerOutput.ll', useMemory = False):
 
     ast = importMiniFile(miniFile)
 
     retType = typeCheckProgram(ast)
 
-    if retType != -1:
-        code = toLLVM(ast)
+    if useMemory:
+        if retType != -1:
+            code = toLLVM(ast)
 
-        print(code)
+            print(code)
+    else:
+        if retType != -1:
+            code = topSSACompile(ast)
+
+            print(code)
     
 def importMiniFile(filepath) -> m_prog:
     with open('tmp', 'w+') as outputHolder:
@@ -28,9 +35,17 @@ def importMiniFile(filepath) -> m_prog:
     return parse(jsonAST)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 3:
+    if len(sys.argv) > 4:
         print(f'ERROR: got  {len(sys.argv)} arguments, expected <= 3')
 
-    # print(*sys.argv[1:])
-    top_compile(*sys.argv[1:])
+    match sys.argv:
+        case [pythonFile, miniFile]:
+            top_compile(*sys.argv[1:])
+        case [pythonFile, miniFile, outputFile]:
+            top_compile(*sys.argv[1:])
+        case [pythonFile, miniFile, outputFile, '-stack']:
+            top_compile(*sys.argv[1:-1], useMemory=True)
+        case [pythonFile, miniFile, '-stack']:
+            top_compile(*sys.argv[1:-1], useMemory=True)
+
     # top_compile('miniFiles/dot.mini')
